6fede72499c98da3f623a37165510f8a29a9619a
bushraYounes <boushra.ali.younes@gmail.com>
2024-05-05 23:58:28 +0300

    index and store methodes

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Traits\ApiResponseTrait;
-use App\Http\Traits\ImageTrait;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Http\Traits\ImageTrait;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Http\Traits\ApiResponseTrait;
+use App\Http\Resources\ProjectResource;
+use App\Http\Requests\ProjectStoreRequest;
 
 class ProjectController extends Controller
 {
@@ -15,15 +19,41 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $projects = Project::all();
+            return $this->customeResponse(ProjectResource::collection($projects), 'Success', 200);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return $this->responseJson(null, "Failed", 400);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProjectStoreRequest $request)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+
+            //store image path using ImageTrait Function
+            $image_path = $this->storeImage($request->image, '/images/');
+
+            $project = Project::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'year' => $request->year,
+                'image' => $image_path,
+                'category' => $request->category
+            ]);
+            DB::commit();
+            return $this->customeResponse(new ProjectResource($project), 'Store Success', 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error($th);
+            return $this->customeResponse(null, 'Store Failed', 200);
+        }
     }
 
     /**
