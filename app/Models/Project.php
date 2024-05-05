0b5fbe43cec17fd1724b6c52bf048ae928d2972e
bushraYounes <boushra.ali.younes@gmail.com>
2024-05-05 23:47:45 +0300

    Project model with setter and getter

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'year',
+        'image',
+        'category',
+    ];
+
+    public function getCategoryAttribute($value)
+    {
+        return ucfirst($value);
+    }
+    public function setCategoryAttribute($value)
+    {
+        $this->attributes['category'] = ucfirst($value);
+    }
+   
 }
