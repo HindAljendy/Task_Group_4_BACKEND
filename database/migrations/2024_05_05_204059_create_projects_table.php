92ef8cafec892f763216260d02fcba82e7c40c48
bushraYounes <boushra.ali.younes@gmail.com>
2024-05-05 23:44:30 +0300

    Project migration

--- database/migrations/2024_05_05_204059_create_projects_table.php
+++ database/migrations/2024_05_05_204059_create_projects_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('year');
+            $table->string('image');
+            $table->string('category');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
